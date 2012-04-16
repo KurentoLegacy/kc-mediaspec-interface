b32d9fddf854701ad1c099e365f2158feaaa1774
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-04-16 15:40:59 +0200

    Add Direction to mediaSpec

--- src/main/thrift/mediaSpec.thrift
+++ src/main/thrift/mediaSpec.thrift
@@ -8,8 +8,16 @@ enum MediaType {
 	VIDEO
 }
 
+enum Direction {
+	SEND,
+	RECV,
+	SEND_RECV,
+	INACTIVE,
+}
+
 struct MediaSpec {
 	1: required list<payload.Payload> payloads,
 	2: required set<MediaType> type,
 	3: required transport.Transport transport,
+	4: required Direction direction,
 }
