d915a08722c55a86a16bfb7424cbb21180e890d3
Miguel París Díaz <mparisdiaz@gmail.com>
2012-07-05 18:48:31 +0200

    Complete intersection test.

--- kis-mediaspec/kis-mediaspec-test/IntersectTestCase.m
+++ kis-mediaspec/kis-mediaspec-test/IntersectTestCase.m
@@ -113,7 +113,7 @@
 			[medias addObject:media];
 		}
 		
-		ssOfferer = [SpecTools createSessionSpecWithMedias:medias id:@"12345"];
+		ssOfferer = [SpecTools createSessionSpecWithMedias:medias id:@"99991"];
 	}
 	
 	KisSessionSpec *ssAnswerer;
@@ -159,15 +159,94 @@
 			[medias addObject:media];
 		}
 		
-		ssAnswerer = [SpecTools createSessionSpecWithMedias:medias id:@"12345"];
+		ssAnswerer = [SpecTools createSessionSpecWithMedias:medias id:@"99992"];
 	}
 	
-	KisSessionSpec *ssIntersectResult = [SpecTools intersectSessionSpecsAnswerer:ssAnswerer
+	
+	KisSessionSpec *ssExpectedAnswererResult;
+	{
+		NSMutableArray *medias = [[NSMutableArray alloc] init];
+		
+		{
+			NSMutableArray *payloads = [[NSMutableArray alloc] init];
+			NSSet *types = [NSSet setWithObject:[NSNumber numberWithInt:MediaType_VIDEO]];
+			
+			KisTransportRtp *tRtp = [[KisTransportRtp alloc] initWithAddress:@"127.0.0.2" port:2222];
+			KisTransport *transport = [[KisTransport alloc] initWithRtp:tRtp rtmp:nil];
+			
+			KisPayloadRtp *payRtp = [SpecTools createPayloadRtpWithId:97 codecName:@"MP4V-ES" clockRate:90000];
+			[payRtp setBitrate:200];
+			[payRtp setExtraParams:[[NSDictionary alloc] init]];
+			KisPayload *pay = [[KisPayload alloc] initWithRtp:payRtp];
+			[payloads addObject:pay];
+			
+			KisMediaSpec *media = [[KisMediaSpec alloc] initWithPayloads:payloads type:types transport:transport direction:Direction_SENDRECV];
+			[medias addObject:media];
+		}
+		
+		{
+			NSMutableArray *payloads = [[NSMutableArray alloc] init];
+			NSSet *types = [NSSet setWithObject:[NSNumber numberWithInt:MediaType_AUDIO]];
+			
+			KisTransportRtp *tRtp = [[KisTransportRtp alloc] initWithAddress:@"127.0.0.2" port:3332];
+			KisTransport *transport = [[KisTransport alloc] initWithRtp:tRtp rtmp:nil];
+			
+			KisMediaSpec *media = [[KisMediaSpec alloc] initWithPayloads:payloads type:types transport:transport direction:Direction_INACTIVE];
+			[medias addObject:media];
+		}
+		
+		ssExpectedAnswererResult = [SpecTools createSessionSpecWithMedias:medias id:@"99991"];
+	}
+	
+	KisSessionSpec *ssExpectedOffererResult;
+	{
+		NSMutableArray *medias = [[NSMutableArray alloc] init];
+		
+		{
+			NSMutableArray *payloads = [[NSMutableArray alloc] init];
+			NSSet *types = [NSSet setWithObject:[NSNumber numberWithInt:MediaType_VIDEO]];
+			
+			KisTransportRtp *tRtp = [[KisTransportRtp alloc] initWithAddress:@"127.0.0.1" port:2221];
+			KisTransport *transport = [[KisTransport alloc] initWithRtp:tRtp rtmp:nil];
+			
+			KisPayloadRtp *payRtp = [SpecTools createPayloadRtpWithId:97 codecName:@"MP4V-ES" clockRate:90000];
+			[payRtp setBitrate:200];
+			[payRtp setExtraParams:[[NSDictionary alloc] init]];
+			KisPayload *pay = [[KisPayload alloc] initWithRtp:payRtp];
+			[payloads addObject:pay];
+			
+			KisMediaSpec *media = [[KisMediaSpec alloc] initWithPayloads:payloads type:types transport:transport direction:Direction_SENDRECV];
+			[medias addObject:media];
+		}
+		
+		{
+			NSMutableArray *payloads = [[NSMutableArray alloc] init];
+			NSSet *types = [NSSet setWithObject:[NSNumber numberWithInt:MediaType_AUDIO]];
+			
+			KisTransportRtp *tRtp = [[KisTransportRtp alloc] initWithAddress:@"127.0.0.1" port:3331];
+			KisTransport *transport = [[KisTransport alloc] initWithRtp:tRtp rtmp:nil];
+			
+			KisMediaSpec *media = [[KisMediaSpec alloc] initWithPayloads:payloads type:types transport:transport direction:Direction_INACTIVE];
+			[medias addObject:media];
+		}
+		
+		ssExpectedOffererResult = [SpecTools createSessionSpecWithMedias:medias id:@"99991"];
+	}
+	
+	
+	
+	
+	NSArray *ssIntersectResult = [SpecTools intersectSessionSpecsAnswerer:ssAnswerer
 									andOfferer:ssOfferer];
 	
 	NSLog(@"ssOfferer: %@", ssOfferer);
 	NSLog(@"ssAnswerer: %@", ssAnswerer);
 	NSLog(@"ssIntersectResult: %@", ssIntersectResult);
+	
+	STAssertTrue([[[ssIntersectResult objectAtIndex:0] description] isEqualToString:ssExpectedAnswererResult.description],
+		     @"Answerer result fails");
+	STAssertTrue([[[ssIntersectResult objectAtIndex:1] description] isEqualToString:ssExpectedOffererResult.description],
+		     @"Offerer result fails");
 }
 
 @end
