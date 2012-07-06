1dae2f57ed4e528aff0f7c3381a1b1f14c690436
Miguel París Díaz <mparisdiaz@gmail.com>
2012-07-06 13:34:54 +0200

    Add SdpConversor in kis-mediaspec.

---
+++ kis-mediaspec/SdpConversor.h
@@ -0,0 +1,13 @@
+//
+//  SdpConversor.h
+//  kis-mediaspec
+//
+//  Created by Miguel París on 06/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SdpConversor : NSObject
+
+@end
---
+++ kis-mediaspec/SdpConversor.m
@@ -0,0 +1,13 @@
+//
+//  SdpConversor.m
+//  kis-mediaspec
+//
+//  Created by Miguel París on 06/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SdpConversor.h"
+
+@implementation SdpConversor
+
+@end
